a9139dc2db05ebd639c4a6b661a9eeb36894b83b
Dmitry Redkin <tanatonaut@gmail.com>
2024-05-17 00:28:36 +0300

    final changes

--- deploy/1_UniswapV2.migration.js
+++ deploy/1_UniswapV2.migration.js
@@ -1,5 +1,6 @@
 const hre = require("hardhat");
 const { Deployer, Reporter } =require("@solarity/hardhat-migrate");
+require('dotenv').config();
 
 // Comment toolbox and uncomment migrate and waffle inside hardhat.config.js
 module.exports = async (deployer) => {
@@ -7,5 +8,14 @@ module.exports = async (deployer) => {
 
   const [DEPLOYER] = await hre.ethers.getSigners();
   
-  await deployer.deploy(Factory, [DEPLOYER.address]);
+  const factory = await deployer.deploy(Factory, [DEPLOYER.address]);
+
+  const WETH = process.env.WETH;
+
+  const UniswapV2Router01 = await hre.ethers.getContractFactory("UniswapV2Router01");
+  const UniswapV2Router02 = await hre.ethers.getContractFactory("UniswapV2Router02");
+
+  const router1 = await deployer.deploy(UniswapV2Router01, [factory.address, WETH]);
+  const router2 = await deployer.deploy(UniswapV2Router02, [factory.address, WETH]);
+  
 };
